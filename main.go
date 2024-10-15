8eb3d49936dc9238e957ea4b00511d3f8cb4bc15
vyhcn3012 <vyhcn30122002@gmail.com>
2024-10-15 21:00:40 +0700

    [swag] init

--- main.go
+++ main.go
@@ -6,14 +6,32 @@ import (
 	"gomodule/libs"
 
 	"github.com/gin-gonic/gin"
+
+	_ "gomodule/docs"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title User API
+// @version 1.0
+// @description This is a sample server for managing users.
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
 func main() {
 	// Create a gin router
 	r := gin.Default()
 
 	// Register the user module
 	libs.RegisterModule(r, AppModule())
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	// Start the gin server
 	if err := r.Run(":8080"); err != nil {
--- users/users.controller.go
+++ users/users.controller.go
@@ -16,17 +16,36 @@ func UserController() *libs.CtrlController {
 	return userController
 }
 
+// UserController godoc
+// @Summary Get user by ID
+// @Description Get user details
+// @Tags users
+// @Param id path string true "User ID"
+// @Success 200 {string} string "Get User {id}"
+// @Router /users/{id} [get]
 func getUser(c *gin.Context) interface{} {
 	id := c.Param("id")
 	return "Get User " + id
 }
 
+// getUsers godoc
+// @Summary Get all users
+// @Description Get a list of users
+// @Tags users
+// @Success 200 {object} map[string]interface{}
+// @Router /users/ [get]
 func getUsers(c *gin.Context) interface{} {
 	return map[string]interface{}{
 		"data": 1,
 	}
 }
 
+// createUser godoc
+// @Summary Create a new user
+// @Description Create a new user
+// @Tags users
+// @Success 200 {string} string "Create User"
+// @Router /users/ [post]
 func createUser(c *gin.Context) interface{} {
 	return "Create User"
 }
